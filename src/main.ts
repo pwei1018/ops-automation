cd899788b71e551e53bc4ebea060ef1fc08b4b24
pwei1018 <patrick.wei@gov.bc.ca>
2021-09-17 11:39:14 -0700

    u

--- src/main.ts
+++ src/main.ts
@@ -29,10 +29,3 @@ async function run (): Promise<void> {
 
 run()
 
-
-//'5c768e1205971f0c9aa07e193fe151ce1b9d18efbd855cd2875dec918b7a1d72394068b10261360c',
-//  '158729774',
-//  '6125d6774f702c00193cafae',
-//  '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',
-//  'pwei1018',
-//  'entity'
